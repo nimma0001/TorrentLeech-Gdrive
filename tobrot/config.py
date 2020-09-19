0308b6b432d0ea7075f7b7d2192e7e48a9ed4acb
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2020-09-19 08:49:44 +0530

    Update and rename sample_config.py to config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -9,7 +9,7 @@ class Config(object):
     OWNER_ID = 1111350430
     # Get these values from my.telegram.org
     # to store the channel ID who are authorized to use the bot
-    AUTH_CHANNEL = -1001153895452 1111350430 1219481510 691522795 -1001398749619"
+    AUTH_CHANNEL = [-1001153895452 1111350430 1219481510 691522795 -1001398749619]
     # the download location, where the 
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
@@ -42,17 +42,13 @@ class Config(object):
     LEECH_COMMAND = "leech@speedlestbot"
     YTDL_COMMAND = "ytdl@speedlestbot"
     RCLONE_CONFIG = "type = drive
-
 client_id = 472268816510-rtc4kmaeb4620fn0163pn2gjid2noohe.apps.googleusercontent.com
-
 client_secret = zGz5xkWy-oKfsXrplvoAmkbL
-
 scope = drive
-
-token = {"access_token":"ya29.a0AfH6SMDyKvvDGv8qWxpOckzTaaEW4D9NIYfE0eR6pZj7vLZEZsFnm8-x9vNAgymuZuy8JQ86FKdmJnuyNfxXKRr-zeXsAExs33vV7kYKrSD02SC0aIVek7ycFSzIVf-RyW0BlBIaUeVzpncVFX3JBOZ45sPeK3soWD8","token_type":"Bearer","refresh_token":"1//0gT8JP3cD8fhmCgYIARAAGBASNwF-L9IraC6WmaP3cnkdg1Po-nJGYB27zClC7pjmYDz1j_QRZeV16qXwIyZINEx4Dxd__e7s1c0","expiry":"2020-07-08T16:05:55.049773068Z"}'
+token = {"access_token":"ya29.a0AfH6SMDyKvvDGv8qWxpOckzTaaEW4D9NIYfE0eR6pZj7vLZEZsFnm8-x9vNAgymuZuy8JQ86FKdmJnuyNfxXKRr-zeXsAExs33vV7kYKrSD02SC0aIVek7ycFSzIVf-RyW0BlBIaUeVzpncVFX3JBOZ45sPeK3soWD8","token_type":"Bearer","refresh_token":"1//0gT8JP3cD8fhmCgYIARAAGBASNwF-L9IraC6WmaP3cnkdg1Po-nJGYB27zClC7pjmYDz1j_QRZeV16qXwIyZINEx4Dxd__e7s1c0","expiry":"2020-07-08T16:05:55.049773068Z"}"
     DESTINATION_FOLDER = "Movie"
     GLEECH_COMMAND = "gleech@speedlestbot"
-    INDEX_LINK = os.environ.get("INDEX_LINK", "")
+    INDEX_LINK = "https://folderdownload.freemovies.workers.dev/Exoticaz"
     TELEGRAM_LEECH_COMMAND_G = os.environ.get("TELEGRAM_LEECH_COMMAND_G", "tleech")
     CANCEL_COMMAND_G = os.environ.get("CANCEL_COMMAND_G", "cancel")
     GET_SIZE_G = os.environ.get("GET_SIZE_G", "getsize")
